2a8f297c96aea0eeb646f598afeac9852d8f2cbf
Agbodjive Etorna k <akietorna@gmail.com>
2021-10-11 12:07:41 +0000

    practice

--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function Items(props){
+  return <div className="items">
+    <b>Name:</b>{props.name} 
+    <b>Price:</b> {props.price}
+  </div>
+}
+
+const cheese=<Items name="Cheese" price="GHC 15.50"/>;
+const bread=<Items name="Bread" price="GHC 10.00"/>
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  cheese,
+  bread,
+  document.getElementById('testing')
 );
 
 // If you want to start measuring performance in your app, pass a function
