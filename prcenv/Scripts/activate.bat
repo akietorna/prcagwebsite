4f319b4a7329b6971e763b1c5439d4b2d3be682a
akietorna <akietorna@gmail.com>
2022-07-04 17:05:39 +0100

    update

--- prcenv/Scripts/activate.bat
+++ prcenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Agbodjive Etorna\Desktop\myapp\prcenv
+set VIRTUAL_ENV=C:\Users\Akietorna\Desktop\myapp\prcenv
 
 if not defined PROMPT set PROMPT=$P$G
 
